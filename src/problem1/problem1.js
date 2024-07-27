3af9c83df6b2d2c64c9e1f7ed252e6ab3b8408a5
minhle106cse <80529019+minhle106cse@users.noreply.github.com>
2024-07-27 15:24:13 +0700

    Update problem1.js

--- src/problem1/problem1.js
+++ src/problem1/problem1.js
@@ -11,7 +11,7 @@ var sum_to_n_a = function (n) {
 var sum_to_n_b = function (n) {
 	if (n == 1) return n
 
-	return n + sum_to_n_c(n - 1)
+	return n + sum_to_n_b(n - 1)
 }
 
 var sum_to_n_c = function (n) {
